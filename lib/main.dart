d4cbb52cdaefaae1c174ff98cf4f5851beb6c754
Glushenkov <romaglushenkov02@gmail.com>
2024-04-03 01:19:31 +0400

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:car_rent_app/car_rent_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(CarRentApp());
+}
