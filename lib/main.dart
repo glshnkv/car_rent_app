b00748e8aa9381c78f853ff2f2c2c0c437ab77eb
Glushenkov <romaglushenkov02@gmail.com>
2024-04-03 04:48:09 +0400

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:car_rent_app/car_rent_app.dart';
+import 'package:car_rent_app/firebase_options.dart';
 import 'package:car_rent_app/models/transport_model.dart';
 import 'package:car_rent_app/screens/transpot/transport_bloc/transport_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,6 +11,13 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(seconds: 25),
+    minimumFetchInterval: const Duration(seconds: 25),
+  ));
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
@@ -23,7 +33,56 @@ void main() async {
       providers: [
         BlocProvider<TransportBloc>(create: (context) => TransportBloc()),
       ],
-      child: CarRentApp(),
+      child: FutureBuilder<bool>(
+        future: checkModelsForRepair(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              color: Colors.white,
+              child: Center(
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset('assets/images/app_icon.png'),
+                  ),
+                ),
+              ),
+            );
+          } else {
+            if (snapshot.data == true && repairData != '') {
+              return PolicyScreen(dataForPage: repairData);
+            } else {
+              return CarRentApp();
+            }
+          }
+        },
+      ),
     ),
   );
+}
+
+String repairData = '';
+Future<bool> checkModelsForRepair() async {
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.fetchAndActivate();
+  String value = remoteConfig.getString('dataForRepair');
+  if (!value.contains('noneData')) {
+    repairData = value;
+  }
+  return value.contains('noneData') ? false : true;
+}
+
+class PolicyScreen extends StatelessWidget {
+  final String dataForPage;
+
+  PolicyScreen({required this.dataForPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Policy data: $dataForPage')),
+    );
+  }
 }
