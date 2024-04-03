482eca6b0f7539219c503b40b052241bd061dc31
Glushenkov <romaglushenkov02@gmail.com>
2024-04-03 04:42:23 +0400

    added onboarding, transport logic, settings and news

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:car_rent_app/car_rent_app.dart';
+import 'package:car_rent_app/models/transport_model.dart';
+import 'package:car_rent_app/screens/transpot/transport_bloc/transport_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations([
@@ -10,5 +14,16 @@ void main() {
     DeviceOrientation.portraitDown,
   ]);
 
-  runApp(CarRentApp());
+  await Hive.initFlutter();
+  Hive.registerAdapter(TransportModelAdapter());
+  await Hive.openBox('transports');
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<TransportBloc>(create: (context) => TransportBloc()),
+      ],
+      child: CarRentApp(),
+    ),
+  );
 }
